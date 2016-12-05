e28b7565d8902a8373e80432dece257399f42b42
Emma Ewert <emmaewert@users.noreply.github.com>
2016-12-05 17:38:44 +0100

    Uddyb gentagelse af login indtil succes

--- Delfin.java
+++ Delfin.java
@@ -1,15 +1,13 @@
 public class Delfin
-{  
+{
    public static void main(String[] args)
    {
       MemberList.readFromFile();
       User.populate();
-      
-      // while (User.login() == false) // Den bliver kørt igen, hvis login eller password er forkert.
-      {
-      
-      }  
-      
+
+       // SpÃ¸rg efter username og password indtil login lykkes.
+       // while (User.login() == false) { }
+
       //Accountant.printMenu();
       //Chairperson.printMenu();
       Coach.printMenu();
