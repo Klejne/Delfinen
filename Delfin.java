bc3d3af0a4a51731ee0b3a3f6e2f0039503d104e
Emma Ewert <emmaewert@users.noreply.github.com>
2016-12-05 20:37:38 +0100

    Gennemgå hver menu efter den forrige

--- Delfin.java
+++ Delfin.java
@@ -8,8 +8,8 @@ public class Delfin
        // Spørg efter username og password indtil login lykkes.
        // while (User.login() == false) { }
 
-      //Accountant.printMenu();
-      //Chairperson.printMenu();
+      Accountant.printMenu();
+      Chairperson.printMenu();
       Coach.printMenu();
    }
 }
