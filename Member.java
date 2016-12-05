78a2093ec03a6d6cb9bc76ebb79a65e8ccbc2f0b
Emma Ewert <emmaewert@users.noreply.github.com>
2016-12-05 21:50:36 +0100

    Justér flere variabler

--- Member.java
+++ Member.java
@@ -1,30 +1,29 @@
 import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.GregorianCalendar;
 import java.util.Calendar;
 
 public class Member
 {
-   public int id;
-   public String name;
+   public int      id;
+   public String   name;
    public Calendar birthday; // Vi kan bruge Calendar, da det er en superklasse af GregorianCalendar
-   public int paidYear;
+   public int      paidYear;
    public ArrayList<Result> results = new ArrayList();
    
    
    public Member(int id, String name, Calendar birthday, int paidYear)  //constructor
    {
-      this.id = id;
-      this.name = name;
+      this.id       = id;
+      this.name     = name;
       this.birthday = birthday;
       this.paidYear = paidYear;
    }
    
-   
-   public String toString()   //kaldes automatisk de steder hvor objektet konverteres til en streng
+   /*
+      Kaldes automatisk de steder hvor objektet konverteres til en streng, fx når det printes.
+    */
+   public String toString()
    {
       String string;
-
       string = String.format("%-4d %-15s %02d/%02d/%d %d",
          id,
          name,
@@ -41,9 +40,9 @@ public class Member
    public int bestTime(String discipline)
    {
       int bestTime;
-      
       bestTime = Integer.MAX_VALUE;
-      
+
+      // For hvert `result` af typen `Result` i `ArrayList`en `results`
       for (Result result : results)
       {
          if (result.time < bestTime)
@@ -53,8 +52,5 @@ public class Member
       }
       
       return bestTime;
-      
    }
-   
-   
 }
